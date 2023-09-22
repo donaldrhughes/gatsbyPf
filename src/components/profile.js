fe15eb26e96737acbd6073e726c8fd61a1c15e9d
Donald Hughes <donaldrhughes@gmail.com>
2023-09-22 14:11:20 -0700

    sandbox copy

--- src/components/profile.js
+++ src/components/profile.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types"
 import styles from "../../styles/reactStyles"
 
 const Profile = () => {
-  useEffect(() => {}, [])
+  // useEffect(() => {}, [])
 
   return (
     <>
--- src/components/sandbox.js
+++ src/components/sandbox.js
@@ -1,28 +1,23 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { AppContext } from "../context/app-context"
+import React from "react";
+import PropTypes from "prop-types";
+import { AppContext } from "../context/app-context";
+import styles from "../../styles/reactStyles";
 
 const Sandbox = () => (
-    <AppContext.Consumer>
-      {({ siteMetadata }) => {
-        return (
-          <>
-            <p>
-              {" "}
-              Do you own it or lease it local gangs who may know you and they
-              may operate in your area, they may help or hinder your business
-              maybe they conduct their business in your bar -- no guns no droids
-              bartending skill firearms intimidation? management? salvage
-              communications contracts go through a middleman
-            </p>
-          </>
-        )
-      }}
-    </AppContext.Consumer>
-)
+  <AppContext.Consumer>
+    {({ siteMetadata }) => {
+      return (
+        <>
+          <div className={styles.pageH}>Sandbox</div>
+          <>This is the Sandbox</>
+        </>
+      );
+    }}
+  </AppContext.Consumer>
+);
 
-export default Sandbox
+export default Sandbox;
 
 Sandbox.propTypes = {
-    siteMetadata: PropTypes.object,
-  }
+  siteMetadata: PropTypes.object,
+};
